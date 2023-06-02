867b3b9cc457d47ac6dccae2d6b341cfb47acb7d
dteni <113144489+dteni@users.noreply.github.com>
2023-06-02 10:04:09 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def exponent(a, b):
+    return a ^ b
+
+print("Calculator")
+
+num1 = float(int("Enter the first number:"))
+num2 = float(int("Enter the second number:"))
+
+print("Select an option")
+print("1. Addition")
+print("2. Subtraction")
+print()
